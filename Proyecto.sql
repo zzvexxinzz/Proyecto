ae1f18a74bca295cb0bf5c9111700fbd71854bf4
zzvexxinzz <santiago.sanchez.castro339@gmail.com>
2026-05-14 10:43:53 -0600

    sql update

--- Proyecto.sql
+++ Proyecto.sql
@@ -4,7 +4,12 @@ CREATE TABLE especialidades (
     nombre VARCHAR(100) NOT NULL,
     descripcion TEXT
 );
-
+CREATE TABLE api_users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 -- Tabla de doctores
 CREATE TABLE doctores (
     id_doctor SERIAL PRIMARY KEY,
